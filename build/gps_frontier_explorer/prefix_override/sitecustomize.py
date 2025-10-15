29547b964353d66cbc9e8ca3cea962366c3f8292
rakshithrajs <rakshithrajr@gmail.com>
2025-10-16 00:39:57 +0530

    Add .gitignore to ignore build log install

--- build/gps_frontier_explorer/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/raj/ROS2/gps_frontier_exploration/explore_exploration/install/gps_frontier_explorer'
